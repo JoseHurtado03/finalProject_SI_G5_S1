05584fd1b25ff2286a4901bc0997dc53ff08fb40
Joseph Ruiz <josephruiz.molina@gmail.com>
2024-03-27 00:10:53 -0400

    CSS Little details :penguin:

--- src/view/LogIn.jsx
+++ src/view/LogIn.jsx
@@ -1,113 +1,113 @@
-import { useEffect, useState } from "react"
-import {createUserWithEmail,SingInwithEmail,singInGoogle,singOut} from "../Controllers/auth"
-import {useUser} from "../context/user"
-import { useNavigate, Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import {
+  createUserWithEmail,
+  SingInwithEmail,
+  singInGoogle,
+  singOut,
+} from "../Controllers/auth";
+import { useUser } from "../context/user";
+import { useNavigate, Link } from "react-router-dom";
 
-import {createUser,buscarUsuarioPorId} from "../Controllers/usuario"
-import styles from '../CSS/Login.module.css'
+import { createUser, buscarUsuarioPorId } from "../Controllers/usuario";
+import styles from "../CSS/Login.module.css";
 
+export default function Login() {
+  const navigate = useNavigate();
+  const user = useUser();
 
-export default function Login(){
-    const navigate= useNavigate()
-    const user= useUser()
+  function separarNombreApellido(nombreCompleto) {
+    const partes = nombreCompleto.split(" "); // Divide la cadena en un array de substrings separados por espacios
+    const nombre = partes[0]; // El primer elemento es el nombre
+    const apellido = partes.slice(1).join(" "); // Los elementos restantes son el apellido
+    return { nombre, apellido };
+  }
 
-    function separarNombreApellido(nombreCompleto) {
-        
-        const partes = nombreCompleto.split(' '); // Divide la cadena en un array de substrings separados por espacios
-        const nombre = partes[0]; // El primer elemento es el nombre
-        const apellido = partes.slice(1).join(' '); // Los elementos restantes son el apellido
-        return { nombre, apellido };
-    }
+  useEffect(() => {
+    if (user) {
+      const comprove = async (id) => {
+        const nuevo = await buscarUsuarioPorId(id);
 
+        if (nuevo) {
+          console.log("es nuebo");
+          //activar despues
+          navigate("/");
+        } else {
+          const NombreApellido = separarNombreApellido(user.displayName);
 
+          const crear = async () => {
+            await createUser(
+              NombreApellido.nombre,
+              NombreApellido.apellido,
+              "username",
+              user.email,
+              "password"
+            );
+          };
 
+          crear();
 
-
-    useEffect(()=>{
-        
-        if(user){
-            
-            const comprove= async (id)=>{
-
-                const nuevo= await buscarUsuarioPorId(id)
-
-                if(nuevo){
-                    console.log("es nuebo")
-                    //activar despues
-                    navigate("/")
-                }else{
-                    
-                    const NombreApellido=separarNombreApellido(user.displayName)
-          
-            
-                         const crear=async ()=>{await createUser(NombreApellido.nombre,NombreApellido.apellido,"username",user.email,"password",)}
-                                  
-                    crear()
-          
-                    //activar despues
-                    navigate("/")
-                    
-                }
-
-            }
-            comprove(user.email)
-            // navigate("/AppPage")
-
-            
+          //activar despues
+          navigate("/");
         }
-
+      };
+      comprove(user.email);
+      // navigate("/AppPage")
     }
+  }, [user, navigate]);
 
-    ,[user,navigate])
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const handleLogin = async (e) => {
+    const user = await SingInwithEmail(email, password);
+  };
 
+  const handleLogingGoogle = async (e) => {
+    const user = await singInGoogle();
+    console.log(user);
+  };
 
-    
-    const [email,setEmail]=useState("")
-    const [password,setPassword]=useState("")
-   
+  const handleBack = () => {
+    navigate("/");
+  };
 
-    const handleLogin= async (e)=> {
-        const user = await SingInwithEmail(email,password)
-
-        
-      
-        
-    }
-
-
-    const handleLogingGoogle= async (e)=> {
-        const user = await singInGoogle()
-        console.log(user)
-        
-
-    }
-
-    const handleBack= ()=>{
-        navigate("/")
-    }
-    
-
-
-
-    return (
-    <div style={{display: 'flex', flexDirection: 'row'}}>
-        <section className={styles.leftImg}></section>
-        <section style={{marginLeft:'300px'}}>
-            <section style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-                <h1 className={styles.mainTitle}>Inicio de Sesión</h1>
-                <button className={styles.homeButton}>Inicio</button>
-            </section>
-            <section>
-                <input value={email} onChange={e =>  setEmail(e.target.value) } placeholder="Correo Electronico" className={styles.input}></input>
-                <input value={password} onChange={e =>  setPassword(e.target.value) } placeholder="Contrasena" className={styles.input}></input>
-            </section>
-            <section style={{marginBottom:'50px'}}>
-                <button  onClick={ handleLogin} className={styles.mainButton}>LogIn</button>
-                <button  onClick={handleLogingGoogle}>GOOGLE</button>
-                <button  onClick={handleBack}>Regresar</button>
-            </section> 
-            <Link to={'/SignIn'}>¿No tienes una cuenta? Regístrate</Link>
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <section className={styles.leftImg}></section>
+      <section style={{ marginLeft: "50px" }}>
+        <section
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <h1 className={styles.mainTitle}>Inicio de Sesión</h1>
+          <button className={styles.homeButton}>Inicio</button>
         </section>
-    </div>)
+        <section className={styles.fields}>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Correo Electronico"
+            className={styles.input}
+          ></input>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Contrasena"
+            className={styles.input}
+          ></input>
+        </section>
+        <section style={{ marginBottom: "50px" }}>
+          <button onClick={handleLogin} className={styles.mainButton}>
+            LogIn
+          </button>
+          <button onClick={handleLogingGoogle}>GOOGLE</button>
+          <button onClick={handleBack}>Regresar</button>
+        </section>
+        <Link to={"/SignIn"}>¿No tienes una cuenta? Regístrate</Link>
+      </section>
+    </div>
+  );
 }
--- src/view/SignIn.jsx
+++ src/view/SignIn.jsx
@@ -8,22 +8,18 @@ import {
 } from "../Controllers/auth";
 import { useUser } from "../context/user";
 import { createUser } from "../Controllers/usuario";
-import styles from '../CSS/SignIn.module.css'
-import {Link} from "react-router-dom";
+import styles from "../CSS/SignIn.module.css";
+import { Link } from "react-router-dom";
 
 export default function Sign() {
   const navigate = useNavigate();
   const user = useUser();
-  
 
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-
-
 
   const handleSignin = async (e) => {
     const user = await createUserWithEmail(email, password);
@@ -42,8 +38,8 @@ export default function Sign() {
 
   useEffect(() => {
     if (user) {
-        //activar despues 
-    //   navigate("/");
+      //activar despues
+      //   navigate("/");
     }
   }, [user, navigate]);
 
@@ -52,9 +48,25 @@ export default function Sign() {
   };
 
   return (
-    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-      <section style={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '100px'}}>
-        <h1 className={styles.mainTitle}>Registrarse</h1>
+    <div
+      className="all"
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+      }}
+    >
+      <section
+        className="fields"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginLeft: "100px",
+          marginRight: "50px",
+        }}
+      >
+        <h1 className={styles.mainTitle}>Registro</h1>
         <input
           value={name}
           onChange={(e) => setName(e.target.value)}
@@ -89,11 +101,21 @@ export default function Sign() {
           className={styles.input}
           placeholder="Contraseña"
         ></input>
-        <section style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-          <button onClick={handleSignin} className={styles.mainButton}>SIGN IN</button>
-          <button>Registrate con Google</button>
+        <section
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <button onClick={handleSignin} className={styles.mainButton}>
+            SIGN IN
+          </button>
         </section>
-        <Link to={"/Login"} style={{marginTop:'20px'}}>Ya tienes una cuenta? Inicia sesión</Link>
+        <button className={styles.Google}>Registrate con Google</button>
+        <Link to={"/Login"} style={{ marginTop: "20px" }}>
+          ¿Ya tienes una cuenta? Inicia sesión
+        </Link>
       </section>
       <section className={styles.rightImg}></section>
     </div>
