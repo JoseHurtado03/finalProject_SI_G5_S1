aa6775bcea8a195b0cc5aed379e5c370be3b4f4d
Angel Carrero <angel.carrero@correo.unimet.edu.ve>
2024-03-30 15:17:16 -0400

    cambios en registro y login con google, cosita en firebase:)

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth } from "firebase/auth";
+import {GoogleAuthProvider, getAuth } from "firebase/auth";
 import {getFirestore } from "firebase/firestore";
 import {getStorage } from "firebase/storage";
 
@@ -27,3 +27,6 @@ const app = initializeApp(firebaseConfig);
 export const db= getFirestore(app)
 export const auth= getAuth(app)
 export const storage=getStorage(app)
+export const googleProvider = new GoogleAuthProvider();
+googleProvider.addScope("profile");
+googleProvider.addScope("email"); 
